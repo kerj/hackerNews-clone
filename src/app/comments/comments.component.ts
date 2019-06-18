1dde4caf225e3c68fa361844f062bdd75b73e7be
Justin Kerntz and brian hensley <justin+brian@>
2019-06-18 16:21:39 -0700

    add article comment page

--- src/app/comments/comments.component.ts
+++ src/app/comments/comments.component.ts
@@ -1,22 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router'
-import { Location } from '@angular/common'
-import { Article } from '../article.model'
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import { Article } from '../article.model';
+import { ArticleService } from '../article.service';
 
 @Component({
   selector: 'app-comments',
   templateUrl: './comments.component.html',
-  styleUrls: ['./comments.component.css']
+  styleUrls: ['./comments.component.css'],
+  providers: [ArticleService]
 })
 export class CommentsComponent implements OnInit {
   articleId: number = null;
+  articleToDisplay: Article;
 
-  constructor(private route: ActivatedRoute, private location: Location) {  }
+  constructor(
+    private route: ActivatedRoute, 
+    private location: Location,
+    private articleService: ArticleService
+    ) {  }
 
   ngOnInit() {
     this.route.params.forEach((urlParameters) => {
       this.articleId = parseInt(urlParameters['id']);
     });
+    this.articleToDisplay = this.articleService.getArticleById(this.articleId);
   }
 
 }
